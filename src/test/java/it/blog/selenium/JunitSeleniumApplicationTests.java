51e468709912488282c44adc443467b092b5e005
Marco Ghisellini <f5121@seat.it>
2019-08-01 16:21:34 +0200

    code

---
+++ src/test/java/it/blog/selenium/JunitSeleniumApplicationTests.java
@@ -0,0 +1,102 @@
+package it.blog.selenium;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class JunitSeleniumApplicationTests {
+
+	public static Properties prop = null;
+
+	public static class MyHtmlUnitDriver extends HtmlUnitDriver {
+		public MyHtmlUnitDriver(boolean enableJavascript) {
+			super(enableJavascript);
+		}
+
+		public WebClient modifyWebClient(WebClient client) {
+			client.setRefreshHandler(new ThreadedRefreshHandler());
+			return client;
+		}
+	}
+
+	private static Properties readPropertiesFile() {
+		try (InputStream input = JunitSeleniumApplicationTests.class.getClassLoader()
+				.getResourceAsStream("application.properties")) {
+
+			Properties prop = new Properties();
+			prop.load(input);
+			return prop;
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+
+	}
+
+	@BeforeClass
+	public static void setUp() {
+		prop = readPropertiesFile();
+	}
+
+	@Test
+	@Ignore
+	public void testHtmlJs() {
+
+		DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit();
+		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+		MyHtmlUnitDriver driver = new MyHtmlUnitDriver(true);
+
+		driver.get(prop.getProperty("HtmlUnitDriver.url"));
+
+		WebElement htmlTag = driver.findElement(By.id("para"));
+
+		System.out.println(htmlTag.getText());
+
+		assertEquals(htmlTag.getText(), "Js Test");
+
+		driver.quit();
+	}
+
+	@Test
+	public void testPhatomJs() throws InterruptedException {
+		String os = System.getProperty("os.name").toLowerCase();
+
+		if (os.contains("windows"))
+			System.setProperty("phantomjs.binary.path", "driver/phantomjs.exe");
+		else
+			System.setProperty("phantomjs.binary.path", "driver/phantomjs");
+
+		DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+		capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_CUSTOMHEADERS_PREFIX + "Authorization",
+				"Basic " + prop.getProperty("PhantonJsDriver.credentials"));
+
+		WebDriver driver = new PhantomJSDriver(capabilities);
+
+		driver.get(prop.getProperty("PhantonJsDriver.url"));
+
+		assertEquals(driver.getTitle(), prop.getProperty("assertTitle"));
+
+		driver.quit();
+
+	}
+
+}
